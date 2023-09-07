f1d825f8f851d4a32149360a2b37326fab2bb946
davidhalasz <davidhalasz22@gmail.com>
2023-09-07 17:45:43 +0200

    edit cover

--- resources/views/components/cover.blade.php
+++ resources/views/components/cover.blade.php
@@ -1,11 +1,11 @@
 <div class="relative">
-    <div class="w-full h-[600px]">
+    <div class="w-full heroheight">
         <img class="absolute w-full h-full object-cover" src="{{ asset('images/light.jpg') }}" alt="" />
         <div class="absolute w-full h-full backdrop-opacity-50 bg-black/50 flex flex-col items-center justify-center px-2">
-            <div class="text-white flex items-center h-64">
+            <div class="text-white flex items-center h-64 ">
                 <img class="h-full py-2" src="{{asset('logo/kriszvill-logo.png')}}" alt=""> 
             </div>
-            <h1 class="ralewayBold text-xl md:text-2xl text-center mt-4 text-[#4DA7F7]">TELJESKÖRŰ VILLANYSZERELÉS</h1>
+            <h1 class="ralewayBold tracking-wide text-xl md:text-3xl text-center mt-4 text-[#4DA7F7]">TELJESKÖRŰ VILLANYSZERELÉS</h1>
         </div>
     </div>
 </div>
